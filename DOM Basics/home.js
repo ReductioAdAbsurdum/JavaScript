403fed57435d57f16196af6419e44c5ec13069c3
ReductioAdAbsurdum <moonburn.co@gmail.com>
2022-02-10 15:27:07 +0100

    Data types, methods, strings,...

---
+++ DOM Basics/home.js
@@ -0,0 +1,120 @@
+// BASIC STUFF
+
+// console.log("console msg");
+// alert("popup alert msg");
+
+
+// document.getElementById("paragraph").innerHTML = "<h1> HEADER </h1>";
+
+// let age = prompt("What is your age");
+// document.getElementById("paragraph").innerHTML = `<h1> Age of ${age} </h1>`;
+
+
+
+// NUMBERS IN JAVASCRIPT
+
+// let x = 10;
+// let y = 5.5;
+
+// x +=1;
+
+// console.log(x);
+
+
+// FUNCTIONS IN JAVASCRIPT
+
+// fun();
+// function fun()
+// {
+//     console.log("fun: called")
+//     return 5;
+// }
+
+// Function that takes the name and returns name with greating
+
+// /**
+//  * 
+//  * @param {string} name 
+//  * @returns {string}
+//  */
+// // function greeting(name)
+// // {
+// //     return "Hello! " + name;
+// // }
+
+// // console.log(greeting("Sergio"))
+
+// // Function that log's all numbers from zero to and including N
+
+// logToInteger(10);
+
+// /**
+//  * @param {Number} n 
+//  */
+// function logToInteger(n)
+// {
+//     for (let i = 0; i <= n; i++) 
+//     {
+//         console.log(i);
+//     }
+// }
+
+
+// WHILE LOOP
+
+// let x = 100;
+
+// while(x > 0.001)
+// {
+//     console.log(x);
+//     x /= 2;
+// }
+
+// FOR LOOP
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i); 
+// }
+
+
+// -------------DATA TYPES----------------------
+
+// let number = 10.1;
+
+// let bigInt = 10n;
+
+// let symbol = Symbol(1);
+
+// let string = "text";
+
+// let bool = true;
+
+// let undefined;
+
+// let nothing = null;
+
+// let array = [1,2,3,'text'];
+
+// let object = {x: "text", y: 5}
+
+// -------------------STRING-----------------------
+
+// let text = 'sample text';
+// let name = 'Samplename';
+
+// console.log(text + "\n".concat(name)); // new line
+// console.log(text.indexOf("not fount so we get -1")); // find index of substring
+// console.log(text.slice(0,100)); // get substring
+// console.log(text.replace("text","xman")); // find and replace
+// console.log(text.charAt(5));
+/*
+// Split every char
+let textChars = text.split("");
+for (let i = 0; i < textChars.length; i++) 
+{
+    const element = textChars[i];
+    console.log(element);    
+}
+*/
+
+//--------------------ARRAYS------------------------
